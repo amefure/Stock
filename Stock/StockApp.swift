5be1f0f2062f26389213ef5961de0ce136744c2d
amefure <shachi10969@gmail.com>
2023-09-10 21:19:20 +0900

    Initial Commit

---
+++ Stock/StockApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockApp.swift
+//  Stock
+//
+//  Created by t&a on 2023/09/10.
+//
+
+import SwiftUI
+
+@main
+struct StockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
