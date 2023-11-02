e8538bd12feae6af1b4af3c5b1d6ff408d5ddae5
amefure <shachi10969@gmail.com>
2023-11-02 22:57:34 +0900

    WatchOS用ターゲットの追加

---
+++ StockWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockWatch Watch App
+//
+//  Created by t&a on 2023/11/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockWatch Watch App/StockWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockWatchApp.swift
+//  StockWatch Watch App
+//
+//  Created by t&a on 2023/11/02.
+//
+
+import SwiftUI
+
+@main
+struct StockWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
