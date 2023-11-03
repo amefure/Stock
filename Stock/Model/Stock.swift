f86460a41ad2f6371b8b1abe1fa7cf1c72fe1e23
amefure <shachi10969@gmail.com>
2023-11-03 23:05:19 +0900

    StockクラスをJSONへマッピングできるように変換

--- Stock/Model/Stock.swift
+++ Stock/Model/Stock.swift
@@ -8,11 +8,47 @@
 import UIKit
 import RealmSwift
 
-class Stock: Object,ObjectKeyIdentifiable {
+class Stock: Object, ObjectKeyIdentifiable, Codable {
     @Persisted(primaryKey: true) var id: ObjectId
-    @Persisted var name:String = ""
+    @Persisted var name: String = ""
     @Persisted var items = RealmSwift.List<StockItem>()
-    @Persisted var order:Int = 0
+    @Persisted var order: Int = 0
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, items, order
+    }
+
+    convenience init(id: ObjectId, name: String, items: RealmSwift.List<StockItem>, order: Int) {
+        self.init()
+        self.id = id
+        self.name = name
+        self.items = items
+        self.order = order
+    }
+
+    required convenience init(from decoder: Decoder) throws {
+        self.init()
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(ObjectId.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        if let itemsArray = try container.decodeIfPresent([StockItem].self, forKey: .items) {
+            items.removeAll()
+            items.append(objectsIn: itemsArray)
+        }
+        order = try container.decode(Int.self, forKey: .order)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+
+        let itemsArray = Array(items)
+        try container.encode(itemsArray, forKey: .items)
+        try container.encode(order, forKey: .order)
+    }
+
+
     
     public var size: Double {
         return Double(items.count)
--- Stock/Model/StockItem.swift
+++ Stock/Model/StockItem.swift
@@ -9,11 +9,40 @@ import UIKit
 
 import RealmSwift
 
-class StockItem: Object, ObjectKeyIdentifiable {
+class StockItem: Object, ObjectKeyIdentifiable, Codable {
     @Persisted(primaryKey: true) var id: ObjectId
-    @Persisted var name:String = ""
-    @Persisted var flag:Bool = false
-    @Persisted var order:Int = 0
+    @Persisted var name: String = ""
+    @Persisted var flag: Bool = false
+    @Persisted var order: Int = 0
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, flag, order
+    }
+
+    convenience init(id: ObjectId, name: String, flag: Bool, order: Int) {
+        self.init()
+        self.id = id
+        self.name = name
+        self.flag = flag
+        self.order = order
+    }
+
+    required convenience init(from decoder: Decoder) throws {
+        self.init()
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(ObjectId.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        flag = try container.decode(Bool.self, forKey: .flag)
+        order = try container.decode(Int.self, forKey: .order)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(flag, forKey: .flag)
+        try container.encode(order, forKey: .order)
+    }
     
     static var demoItems:StockItem {
         let item = StockItem()
